01d26034c17f369729cd74792971b8503d524f40
arata <150427752+arata42@users.noreply.github.com>
2023-12-06 16:25:00 +0900

    script.js を更新

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ document.addEventListener("DOMContentLoaded", function() {
     if (isMobile) {
         //body.style.touchAction = 'none';
         body.style.zoom = '120%';
-        //watch.style.transform = 'translate(-50%, -50%)';//-69%)';
+        watch.style.transform = 'translate(-50%, -50%)';//-69%)';
     } else {
         body.style.touchAction = 'pinch-zoom';
         body.style.zoom = '100%';
